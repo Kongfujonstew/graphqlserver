200db6a2394a5c6568ff6878cc2a653c8db23cc7
Jonathan Stewart <jmichaelstewart@Jonathans-MacBook.local>
2017-07-22 16:35:43 -0700

    install babel/es6 and connect first graphql query w/ schema etc

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var express = require("express");
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import typeDefs from './graphQL/schema';
+import resolvers from './graphQL/resolvers';
+
+const newSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
 
 var port = process.env.PORT || 3000;
 var app = express();
 
-
 app.use(morgan('dev'));
 
-app.listen(port, function() {
-  console.log("Listening on port " + port);
-})
+app.use('/graphiql', graphiqlExpress({
+  endPointURL: '/graphql'
+}));
 
-app.get('/', function(req, res) {
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: newSchema }));
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
+
+app.get('/', (req, res) => {
   res.send('hello world');
-})
+});
+
